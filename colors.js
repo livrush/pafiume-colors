1d6c0e5dc5d19fda4511c3dc90e534af14809447
Olivia Sabo-Rush <w.phrush@gmail.com>
2018-04-21 11:53:04 -0500

    update colors

--- colors.js
+++ colors.js
@@ -5,11 +5,11 @@ var pafiumeColors = (function () {
     cranberry:   [ '#FFAFAF', '#C95D63', '#803C40', '#462123', ],
     brick:       [ '#FF5D48', '#FF3A20', '#BA2B18', '#66180E', ],
     clementine:  [ '#FF8345', '#E36414', '#A6490F', '#7C370B', ],
-    cashmere:    [ '#FDD692', '#B99C6B', '#8D775F', '#4D4134', ],
-    amber:       [ '#FFE392', '#FFCA2E', '#BA9322', '#745C15', ],
-    pea:         [ '#F3FF92', '#C6CA53', '#739130', '#103900', ],
+    cashmere:    [ '#CCB793', '#B99C6B', '#8D775F', '#4D4134', ],
+    amber:       [ '#FFDD7A', '#FFCA2E', '#BA9322', '#745C15', ],
+    pea:         [ '#D5D881', '#C6CA53', '#739130', '#103900', ],
     mint:        [ '#AEECCD', '#69A197', '#136F63', '#0B3D36', ],
-    sky:         [ '#B1E4E4', '#74A4BC', '#717EC3', '#4A4E69', ],
+    sky:         [ '#B1E4E4', '#74A4BC', '#557889', '#354B56', ],
     sea:         [ '#6BD5D3', '#17BEBB', '#2C8C99', '#29555D', ],
   };
 
