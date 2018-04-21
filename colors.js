c5ce3b85e5d1628c89b9c53709dd0e22050408e1
Olivia Sabo-Rush <w.phrush@gmail.com>
2018-04-21 11:45:31 -0500

    update colors

--- colors.js
+++ colors.js
@@ -1,11 +1,11 @@
 var pafiumeColors = (function () {
   var colors = {
     infinity:    [ '#C9B6BE', '#8E8C97', '#5F6062', '#24232A', ],
-    cashmere:    [ '#FDD692', '#B99C6B', '#8D775F', '#4D4134', ],
     soul:        [ '#B19CAA', '#94778B', '#4E344A', '#270722', ],
-    cranberry:   [ '#FFAFAF', '#C95D63', '#803C40', '#6A0000', ],
-    brick:       [ '#FF5D48', '#FF3A20', '#BA2B18', '#931621', ],
+    cranberry:   [ '#FFAFAF', '#C95D63', '#803C40', '#462123', ],
+    brick:       [ '#FF5D48', '#FF3A20', '#BA2B18', '#66180E', ],
     clementine:  [ '#FF8345', '#E36414', '#A6490F', '#7C370B', ],
+    cashmere:    [ '#FDD692', '#B99C6B', '#8D775F', '#4D4134', ],
     amber:       [ '#FFE392', '#FFCA2E', '#BA9322', '#745C15', ],
     pea:         [ '#F3FF92', '#C6CA53', '#739130', '#103900', ],
     mint:        [ '#AEECCD', '#69A197', '#136F63', '#0B3D36', ],
